436e53d8c27c2ca63c0e643838e76f62d60f1586
Daniel Clarke <u8sand@gmail.com>
2020-05-07 14:18:00 -0400

    Create script for building templates.json

---
+++ compose/build_templates.py
@@ -0,0 +1,17 @@
+import re
+import os
+import json
+import glob
+
+root_dir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+template_path = os.path.join(root_dir, 'templates')
+templates = [
+  dict(
+    path=path,
+    **json.load(open(os.path.join(path, 'template.json'), 'r')),
+  )
+  for path in map(os.path.dirname, glob.glob(os.path.join(template_path, '*', 'template.json')))
+]
+
+if __name__ == '__main__':
+  print(json.dumps(templates))
