637d6a4ec4cba94be5eafbf871f890c70f8b1df8
Daniel Clarke <u8sand@gmail.com>
2020-06-25 13:53:40 -0400

    Added passing tests for merge_j2_directories

--- tests/merge_j2/test_merge_j2.py
+++ tests/merge_j2/test_merge_j2.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 def test_merge_j2():
   from compose.merge_j2 import merge_j2
   test_0 = open(os.path.join(os.path.dirname(__file__), 'test_merge_j2_0.j2'), 'r').read()
@@ -6,3 +9,11 @@ def test_merge_j2():
   result = merge_j2(test_0, test_1)
   print(result)
   assert expectation == result
+
+def test_merge_j2_directories():
+  from compose.merge_j2 import merge_j2_directories
+  test_primary = os.path.join(os.path.dirname(__file__), 'primary')
+  test_override = os.path.join(os.path.dirname(__file__), 'override')
+  test_merged = os.path.join(os.path.dirname(__file__), 'merged')
+  shutil.rmtree(test_merged)
+  merge_j2_directories(test_primary, test_override, test_merged)
