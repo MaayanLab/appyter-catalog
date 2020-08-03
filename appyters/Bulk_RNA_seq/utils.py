7a8ef0e9bb5fe6ca1e8d5ccb832697a544f66d6a
mjjeon <mjjeon@korea.ac.kr>
2020-08-03 18:24:14 -0400

    moved functions into utils.py

--- appyters/Bulk_RNA_seq/utils.py
+++ appyters/Bulk_RNA_seq/utils.py
@@ -16,8 +16,19 @@
 
 from rpy2 import robjects
 from rpy2.robjects import r, pandas2ri
-import warnings
+# Basic libraries
+import pandas as pd
+import os
+import urllib3
+import requests, json
+import sys
+import geode
+import random
+from time import sleep
+import time
 import numpy as np
+import warnings
+import base64  
 
 # Visualization
 import plotly
@@ -43,6 +54,8 @@ import scipy.spatial.distance as dist
 import scipy.stats as ss
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import quantile_transform
+from maayanlab_bioinformatics.dge.characteristic_direction import characteristic_direction
+from maayanlab_bioinformatics.dge.limma_voom import limma_voom_differential_expression
 
 
 def printa(a=1):
@@ -81,6 +94,41 @@ def qnormalization(data):
     newdata.columns = data.columns
     newdata.index = data.index
     return newdata  
+
+def normalize(dataset, logCPM_normalization, log_normalization, z_normalization, q_normalization):
+    normalization = 'rawdata'
+    if logCPM_normalization == True:  
+        data = dataset[normalization]
+        normalization += '+logCPM'
+        dataset[normalization] = logCPM(data)
+        
+    if log_normalization == True:    
+        data = dataset[normalization]
+        normalization += '+log'
+        dataset[normalization] = log(data)
+        
+    if z_normalization == True:
+        data = dataset[normalization]
+        normalization += '+z_norm'    
+        dataset[normalization] = data.T.apply(ss.zscore, axis=0).T.dropna()
+
+    if q_normalization == True:
+        data = dataset[normalization]
+        normalization += '+q_norm'
+        dataset[normalization] = qnormalization(data)
+    return dataset, normalization
+
+def create_download_link( df, title = "Download CSV file", filename = "data.csv"):  
+    csv = df.to_csv()
+    b64 = base64.b64encode(csv.encode())
+    payload = b64.decode()
+    html = '<a download="{filename}" href="data:text/csv;base64,{payload}" target="_blank">{title}</a>'
+    html = html.format(payload=payload,title=title,filename=filename)
+    return HTML(html)
+
+def display_link(url):
+    raw_html = '<a href="%s" target="_blank">%s</a>' % (url, url)
+    return display(HTML(raw_html))
 
 def run_pca(dataset, meta_id_column_name, normalization='logCPM', nr_genes=2500, color_by='auto', color_type='categorical', filter_samples=True, plot_type='interactive'):
     # Get data
@@ -223,11 +271,55 @@ def plot_pca(pca_results, meta_id_column_name, meta_class_column_name,plot_type=
         py.image.ishow(fig)
         
         
+def run_clustergrammer(dataset, meta_class_column_name, normalization='logCPM', z_score=True, nr_genes=1500, metadata_cols=None, filter_samples=True,gene_list=None):
+    # Subset the expression DataFrame using top 800 genes with largest variance
+    data = dataset[normalization].copy()
+    variances = np.var(data, axis=1)
+    srt_idx = variances.argsort()[::-1]
+    if gene_list == None or len(gene_list) == 0:
+        expr_df_sub = data.iloc[srt_idx].iloc[:nr_genes]
+    else:
+        gene_list = gene_list.split("\n")
+        common_gene_list = list(set(gene_list).intersection(set(data.index)))
+        expr_df_sub = data.loc[common_gene_list, :]
+        assert len(expr_df_sub.index) > 0
+    
+    # prettify sample names
+    sample_names = ['::'.join([y, x]) for x,y in
+                       zip(dataset["dataset_metadata"][meta_class_column_name], expr_df_sub.columns)]
+    expr_df_sub.columns = sample_names
+    expr_df_sub.index = ["Gene: "+str(x) for x in expr_df_sub.index]
+    sample_name = ["Sample: "+x for x in sample_names]
+    expr_df_sub.columns = sample_name
+
+
+    treatment_type = ["Class: "+ x.split("::")[1] for x in sample_names]
+    new_series = pd.DataFrame(treatment_type).T
+    new_series.columns = expr_df_sub.columns
+    expr_df_sub = pd.concat([new_series, expr_df_sub], axis=0)
+
+    index_list = list(expr_df_sub.index)
+    index_list = ["" if "Gene" not in str(x) else x for x in index_list]
+    expr_df_sub.index = index_list
+    expr_df_sub_file = "expr_df_sub_file.txt"
+    expr_df_sub.to_csv("expr_df_sub_file.txt", sep='\t')
+    # POST the expression matrix to Clustergrammer and get the URL
+    clustergrammer_url = 'https://amp.pharm.mssm.edu/clustergrammer/matrix_upload/'
+    r = requests.post(clustergrammer_url, files={'file': open(expr_df_sub_file, 'rb')}).text
+    return r
+    
+#############################################
+########## 2. Plot
+#############################################
+
+def plot_clustergrammar(clustergrammer_url):
+    clustergrammer_url = clustergrammer_url.replace("http:", "https:")
+    display_link(clustergrammer_url)
+    # Embed
+    display(IPython.display.IFrame(clustergrammer_url, width="1000", height="1000"))
 
 
 robjects.r('''limma <- function(rawcount_dataframe, design_dataframe, filter_genes=FALSE, adjust="BH") {
-    #print(rawcount_dataframe)
-    #print(design_dataframe)
     # Load packages
     suppressMessages(require(limma))
     suppressMessages(require(edgeR))
@@ -265,8 +357,98 @@ robjects.r('''limma <- function(rawcount_dataframe, design_dataframe, filter_gen
     # Get results
     limma_dataframe <- topTable(fit2, adjust=adjust, number=nrow(rawcount_dataframe))
     
-
     # Return
-    return(limma_dataframe)
+    results <- list("limma_dataframe"= limma_dataframe, "rownames"=rownames(limma_dataframe))
+    return (results)
 }
 ''')
+def get_signatures(classes, dataset, normalization, method, meta_class_column_name, meta_id_column_name):
+    expr_df = dataset['rawdata']
+    meta_df = dataset["dataset_metadata"]
+    signatures = dict()
+
+    for cls1, cls2 in combinations(classes, 2):
+        cls1_sample_ids = dataset["dataset_metadata"].loc[dataset["dataset_metadata"][meta_class_column_name]==cls1, meta_id_column_name].tolist()
+        cls2_sample_ids = dataset["dataset_metadata"].loc[dataset["dataset_metadata"][meta_class_column_name]==cls2, meta_id_column_name].tolist()
+        signature_label = " vs. ".join([cls1, cls2])
+
+        if method == "limma":
+            design_dataframe = pd.DataFrame([{'index': x, 'A': int(x in cls1_sample_ids), 'B': int(x in cls2_sample_ids)} for x in expr_df.columns]).set_index('index')
+
+            processed_data = {"expression": expr_df, 'design': design_dataframe}
+            limma = robjects.r['limma']
+            limma_results = pandas2ri.conversion.rpy2py(limma(pandas2ri.conversion.py2rpy(processed_data['expression']), pandas2ri.conversion.py2rpy(processed_data['design']), filter_genes=False))
+            
+            signature = pd.DataFrame(limma_results[0])
+            signature.index = limma_results[1]
+            
+        elif method == "characteristic_direction":
+            signature = characteristic_direction(dataset[normalization].loc[:, cls1_sample_ids], dataset[normalization].loc[:, cls2_sample_ids], calculate_sig=True)
+        
+        signatures[signature_label] = signature
+
+    return signatures
+
+# def run_cd(dataset, normalization, meta_class_column_name):
+#     meta_df = dataset['dataset_metadata'].copy()
+#     expr_df = dataset[normalization]
+#     d_platform_cd = {} # to top up/down genes
+#     cd_results = dict()
+
+#     sample_classes = {}
+#     for layout1, layout2 in combinations(meta_df[meta_class_column_name].unique(), 2):
+#         sample_class = np.zeros(expr_df.shape[1], dtype=np.int32)
+#         sample_class[meta_df[meta_class_column_name].values == layout1] = 1
+#         sample_class[meta_df[meta_class_column_name].values == layout2] = 2
+#         diff_gene_set_name = " vs. ".join([layout1, layout2])
+#         sample_classes[diff_gene_set_name] = sample_class
+
+
+#     for platform, sample_class in sample_classes.items():
+
+#         cd_res = geode.chdir(expr_df.values, sample_class, expr_df.index, gamma=.5, sort=True, calculate_sig=False)
+        
+#         cd_coefs = np.array([x[0] for x in cd_res])
+        
+#         cd_coefs_series = pd.DataFrame(cd_coefs)
+        
+        
+        
+#         cd_coefs_series.index = expr_df.index
+        
+#         cd_coefs_series.columns = ["coef"]
+#         cd_results[platform] = pd.concat([cd_coefs_series,], axis=1).sort_values("coef", ascending=False)
+        
+# #         cd_pvalues = np.array([x[2] for x in cd_res])
+# #         cd_pvalues_series = pd.DataFrame(cd_pvalues)
+# #         cd_pvalues_series.index = expr_df.index
+# #         cd_pvalues_series.columns = ["significance"]
+# #         cd_results[platform] = pd.concat([cd_coefs_series, cd_pvalues_series], axis=1).sort_values("coef", ascending=False)
+        
+        
+#     return cd_results
+
+
+# def get_signatures(classes, dataset, normalization, method, meta_class_column_name, meta_id_column_name):
+#     expr_df = dataset['rawdata']
+#     meta_df = dataset["dataset_metadata"]
+#     signatures = dict()
+#     if method == "limma":
+#         for A, B in combinations(classes, 2):
+#             group_A = meta_df.loc[meta_df[meta_class_column_name]==A, meta_id_column_name].tolist()
+#             group_B = meta_df.loc[meta_df[meta_class_column_name]==B, meta_id_column_name].tolist()
+
+#             design_dataframe = pd.DataFrame([{'index': x, 'A': int(x in group_A), 'B': int(x in group_B)} for x in expr_df.columns]).set_index('index')
+
+#             processed_data = {"expression": expr_df, 'design': design_dataframe}
+
+#             limma = robjects.r['limma']
+#             signature = pandas2ri.conversion.rpy2py(limma(pandas2ri.conversion.py2rpy(processed_data['expression']), pandas2ri.conversion.py2rpy(processed_data['design']), filter_genes))
+
+#             signature_label = " vs. ".join([A, B])
+#             signatures[signature_label] = signature.sort_values("t", ascending=False)
+#     elif method == "characteristic_direction":
+#         signatures = run_cd(dataset, normalization, meta_class_column_name)
+#     return signatures
+
+
