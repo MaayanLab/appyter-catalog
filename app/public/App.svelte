7e01352b115850d0d463d082225dd7a5015be89f
Daniel Clarke <u8sand@gmail.com>
2020-08-10 17:26:12 -0400

    Put logo in a container

--- app/public/App.svelte
+++ app/public/App.svelte
@@ -210,19 +210,23 @@
 }
 </style>
 
-<div class="row m-0 p-0">
-  <div class="col-sm-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6 text-center">
-    <h1>
-      <a href="#/">
-        <img
-          src="{require('./images/appyters_logo.svg')}"
-          class="img-fluid w-100 p-2"
-          alt="Appyters"
-        />
-      </a>
-    </h1>
-    <h3 class="card-subtitle mb-2 text-muted">A catalog of appyter notebooks</h3>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6 text-center">
+      <h1>
+        <a href="#/">
+          <img
+            src="{require('./images/appyters_logo.svg')}"
+            class="img-fluid w-100 p-2"
+            alt="Appyters"
+          />
+        </a>
+      </h1>
+      <h3 class="card-subtitle mb-2 text-muted">A catalog of appyter notebooks</h3>
+    </div>
   </div>
+</div>
+<div class="row m-0 p-0">
   <div class="col-sm-12 p-0">
     <hr />
   </div>
