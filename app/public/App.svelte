73801e9b513c7cb2ff745285f5b306e73415cc17
Daniel Clarke <u8sand@gmail.com>
2020-08-12 18:32:13 -0400

    Add loading indicator before get_pagehits

--- app/public/App.svelte
+++ app/public/App.svelte
@@ -170,8 +170,14 @@
   }
 
   // things to do on window load
-  onMount(() => {
-    get_pagehits()
+  let loaded = false
+  onMount(async () => {
+    try {
+      await get_pagehits()
+    } catch (e) {
+      console.error(e)
+    }
+    loaded = true
   })
 </script>
 
@@ -208,6 +214,32 @@
 :global(body) {
   background-color: #f5f5f5;
 }
+
+.text-placeholder {
+  height: 8px;
+  margin-bottom: 5px;
+}
+
+.placeholder {
+  display: inline-block;
+  background-color: #b0c0c7;
+  animation-name: shine;
+  animation-duration: 2.4s;
+  animation-iteration-count: infinite;
+}
+
+@keyframes shine {
+  0% { 
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.35;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
 </style>
 
 <div class="container">
@@ -270,84 +302,121 @@
 </div>
 <div class="container content flex-grow">
   {#if appyter === undefined}
-    <Masonry>
-      {#each searchAppyters(appyterList, search, $hash.params.q, $hash.params.tags) as appyter}
-        <div
-          in:fade="{{duration: 250}}"
-          out:fade="{{duration: 250}}"
-        >
-          <a
-            href="#/{appyter.name}"
-            class="link-unstyled"
+    {#if !loaded}
+      <Masonry>
+        {#each [1, 2, 3] as n}
+          <div 
+            in:fade="{{duration: 250}}"
+            out:fade="{{duration: 250}}"
           >
             <div class="card">
               <div
-                class="card-img-top"
+                class="card-img-top placeholder"
                 style={[
-                  `background-color: ${appyter.color}`,
-                  appyter.image !== undefined ? (
-                    `background-image: url('${localize_appyter_image(appyter)}')`
-                  ) : undefined,
-                  `background-repeat: no-repeat`,
-                  `background-size: cover`,
-                  `background-position: center`,
                   `width: 100%`,
+                  `background-color: grey;`,
                   `padding-top: 56.25%`, // 720 / 1280 = 0.5625, this preserves aspect ratio of div
                 ].filter((el) => el !== undefined).join('; ')}
               ></div>
               <div class="card-body">
-                <h3 class="card-title">{appyter.title}</h3>
-                <div class="d-flex flex-row flex-nowrap pt-1 pb-2">
-                  <div class="d-flex flex-column pr-2">
-                  {#if appyter.views }
-                    <div class="text-grey text-nowrap">
-                      Views: {appyter.views}
-                    </div>
-                  {/if}
-                  {#if appyter.runs }
-                    <div class="text-grey text-nowrap">
-                    Runs: {appyter.runs}
-                    </div>
-                  {/if}
-                  </div>
-                  <div class="d-flex flex-column pl-2">
-                    {#if appyter.form_views }
-                      <div class="text-grey text-nowrap">
-                      Starts: {appyter.form_views}
-                      </div>
-                    {/if}
-                    {#if appyter.persistent_views }
-                      <div class="text-grey text-nowrap">
-                      Retrievals: {appyter.persistent_views}
-                      </div>
-                    {/if}
-                  </div>
-                </div>
-                <p class="card-text">{@html appyter.description_html}</p>
+                <h3 class="card-title">
+                  <div class="text-placeholder w-100"></div>
+                </h3>
+                <p class="card-text">
+                  <div class="text-placeholder placeholder w-100"></div>
+                  <div class="text-placeholder placeholder w-75"></div>
+                  <div class="text-placeholder placeholder w-100"></div>
                 <div class="pb-4 d-flex flex-row flex-wrap">
-                  <span class="badge badge-success m-1 p-1">v{appyter.version}</span>
-                  <a
-                    class="badge badge-secondary m-1 p-1"
-                    href="javascript:"
-                    on:click={() => $hash.params.license = appyter.license}
-                  >{appyter.license}</a>
-                  {#each appyter.tags as tag}
-                    <a
-                      class="badge badge-primary m-1 p-1 text-white"
-                      href="javascript:"
-                      on:click={() => $hash.params.tags = [...($hash.params.tags || '').split(';').filter(t => t !== '' && t !== tag), tag].join(';')}
-                    >{tag}</a>
-                  {/each}
+                  <div class="text-placeholder placeholder"></div>
                 </div>
-                <button class="btn btn-primary btn-sm">
+                <button class="btn btn-secondary btn-sm disabled">
                   Select
                 </button>
               </div>
             </div>
-          </a>
-        </div>
-      {/each}
-    </Masonry>
+          </div>
+        {/each}
+      </Masonry>
+    {:else}
+      <Masonry>
+        {#each searchAppyters(appyterList, search, $hash.params.q, $hash.params.tags) as appyter}
+          <div
+            in:fade="{{duration: 250}}"
+            out:fade="{{duration: 250}}"
+          >
+            <a
+              href="#/{appyter.name}"
+              class="link-unstyled"
+            >
+              <div class="card">
+                <div
+                  class="card-img-top"
+                  style={[
+                    `background-color: ${appyter.color}`,
+                    appyter.image !== undefined ? (
+                      `background-image: url('${localize_appyter_image(appyter)}')`
+                    ) : undefined,
+                    `background-repeat: no-repeat`,
+                    `background-size: cover`,
+                    `background-position: center`,
+                    `width: 100%`,
+                    `padding-top: 56.25%`, // 720 / 1280 = 0.5625, this preserves aspect ratio of div
+                  ].filter((el) => el !== undefined).join('; ')}
+                ></div>
+                <div class="card-body">
+                  <h3 class="card-title">{appyter.title}</h3>
+                  <div class="d-flex flex-row flex-nowrap pt-1 pb-2">
+                    <div class="d-flex flex-column pr-2">
+                    {#if appyter.views }
+                      <div class="text-grey text-nowrap">
+                        Views: {appyter.views}
+                      </div>
+                    {/if}
+                    {#if appyter.runs }
+                      <div class="text-grey text-nowrap">
+                      Runs: {appyter.runs}
+                      </div>
+                    {/if}
+                    </div>
+                    <div class="d-flex flex-column pl-2">
+                      {#if appyter.form_views }
+                        <div class="text-grey text-nowrap">
+                        Starts: {appyter.form_views}
+                        </div>
+                      {/if}
+                      {#if appyter.persistent_views }
+                        <div class="text-grey text-nowrap">
+                        Retrievals: {appyter.persistent_views}
+                        </div>
+                      {/if}
+                    </div>
+                  </div>
+                  <p class="card-text">{@html appyter.description_html}</p>
+                  <div class="pb-4 d-flex flex-row flex-wrap">
+                    <span class="badge badge-success m-1 p-1">v{appyter.version}</span>
+                    <a
+                      class="badge badge-secondary m-1 p-1"
+                      href="javascript:"
+                      on:click={() => $hash.params.license = appyter.license}
+                    >{appyter.license}</a>
+                    {#each appyter.tags as tag}
+                      <a
+                        class="badge badge-primary m-1 p-1 text-white"
+                        href="javascript:"
+                        on:click={() => $hash.params.tags = [...($hash.params.tags || '').split(';').filter(t => t !== '' && t !== tag), tag].join(';')}
+                      >{tag}</a>
+                    {/each}
+                  </div>
+                  <button class="btn btn-primary btn-sm">
+                    Select
+                  </button>
+                </div>
+              </div>
+            </a>
+          </div>
+        {/each}
+      </Masonry>
+    {/if}
   {:else}
     <div class="row">
       <div class="col-sm-12 col-md-6 col-lg-4 align-self-center">
