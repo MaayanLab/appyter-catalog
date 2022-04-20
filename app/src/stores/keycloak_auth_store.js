1e4832520b86f6b67afd206ccd5a1adbfad9e28d
Daniel Clarke <u8sand@gmail.com>
2022-04-20 10:02:07 -0400

    Time out keycloak if it doesn't resolve

--- app/src/stores/keycloak_auth_store.js
+++ app/src/stores/keycloak_auth_store.js
@@ -1,6 +1,7 @@
 import appyterJson from '@/public/appyters.json'
 import { writable } from 'svelte/store'
 import hash from '@/stores/url_hash_store.js'
+import with_timeout from '@/utils/with_timeout'
 
 function keycloak_auth_store(keycloakProps) {
   const initStore = {}
@@ -36,29 +37,28 @@ function keycloak_auth_store(keycloakProps) {
           set({ state: 'guest', keycloak })
         },
       })
-      keycloak.init({
+      const authenticated = await with_timeout(keycloak.init({
         onLoad: 'check-sso',
         silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
         redirectUri: window.location.href + (window.location.href.includes('?') ? '' : '?'),
-      }).then(authenticated => {
-        set({
-          state: authenticated ? 'auth' : 'guest',
-          keycloak,
-        })
-        // cleanup keycloak auth params
-        hash.update($hash => {
-          const params = { ...$hash.params }
-          if ('code' in params) delete params['code']
-          if ('session_state' in params) delete params['session_state']
-          if ('state' in params) delete params['state']
-          return { ...$hash, params }
-        })
-      }).catch(err => {
-        console.error(err)
-        set({ state: 'error', keycloak })
+      }), 5000)
+      set({
+        state: authenticated ? 'auth' : 'guest',
+        keycloak,
+      })
+      // cleanup keycloak auth params
+      hash.update($hash => {
+        const params = { ...$hash.params }
+        if ('code' in params) delete params['code']
+        if ('session_state' in params) delete params['session_state']
+        if ('state' in params) delete params['state']
+        return { ...$hash, params }
       })
     }
-  })()
+  })().catch(err => {
+    console.error(err)
+    set({ state: 'error', keycloak: {} })
+  })
   return { subscribe }
 }
 
