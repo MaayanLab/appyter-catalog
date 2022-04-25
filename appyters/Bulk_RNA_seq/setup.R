0f1aac19a22b8b4d0170d75db5b7ea77864c8da1
Daniel Clarke <u8sand@gmail.com>
2022-04-25 16:37:21 -0400

    [Bulk_RNA_seq]: Fix dependency installation

--- appyters/Bulk_RNA_seq/setup.R
+++ appyters/Bulk_RNA_seq/setup.R
@@ -1,6 +1,7 @@
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
+install.packages('https://cran.r-project.org/src/contrib/Archive/locfit/locfit_1.5-9.4.tar.gz')
 BiocManager::install("limma")
 BiocManager::install("edgeR")
 BiocManager::install("DESeq2")
