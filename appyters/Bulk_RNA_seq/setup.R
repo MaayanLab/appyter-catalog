7c499f686403a8fe9e63e5968f9452c8885f07d8
Daniel Clarke <u8sand@gmail.com>
2022-04-26 12:06:03 -0400

    [Bulk_RNA_seq]: Get proper dependencies installed

--- appyters/Bulk_RNA_seq/setup.R
+++ appyters/Bulk_RNA_seq/setup.R
@@ -1,9 +1,18 @@
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
-install.packages('https://cran.r-project.org/src/contrib/Archive/locfit/locfit_1.5-9.4.tar.gz')
+BiocManager::install(version = '3.9', ask = FALSE)
+
 BiocManager::install("limma")
+
+install.packages('https://cran.r-project.org/src/contrib/Archive/locfit/locfit_1.5-9.4.tar.gz')
 BiocManager::install("edgeR")
+
 BiocManager::install("DESeq2")
 
 install.packages("statmod")
+
+# verify
+require(limma)
+require(edgeR)
+require(DESeq2)
