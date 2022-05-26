ae3f8e6129a372f6f8059c2b33b6dfee368041ad
Daniel Clarke <u8sand@gmail.com>
2022-05-26 19:41:40 -0400

    [Tumor_Gene_Target_Screener]: Add more deps

--- appyters/Tumor_Gene_Target_Screener/setup.R
+++ appyters/Tumor_Gene_Target_Screener/setup.R
@@ -4,7 +4,18 @@ install.packages("RCurl")
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
-BiocManager::install("DESeq2")
+BiocManager::install(version = '3.9', ask = FALSE)
+
 BiocManager::install("limma")
-BiocManager::install("statmod")
+
+install.packages('https://cran.r-project.org/src/contrib/Archive/locfit/locfit_1.5-9.4.tar.gz')
 BiocManager::install("edgeR")
+
+BiocManager::install("DESeq2")
+
+install.packages("statmod")
+
+# verify
+require(limma)
+require(edgeR)
+require(DESeq2)
