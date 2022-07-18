e76621f4a37955143ea982d68cd65f02d7a4d061
giacomo_marino@brown.edu <giacomo_marino@brown.edu>
2022-07-18 14:38:13 -0400

    Update load_static.py

--- appyters/Gene_Expression_by_Tissue/filters/load_static.py
+++ appyters/Gene_Expression_by_Tissue/filters/load_static.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 def load_static(path):
     with (Path(__file__).parent.parent / 'static' / path).open('r') as fr:
-        return json.load(fr)['genes']
+        return json.load(fr)
