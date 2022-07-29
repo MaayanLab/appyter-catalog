b7f71e811ca074d782a516dc3227f737e10c8966
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-07-29 19:54:36 -0400

    add genome selection

--- appyters/ChIP_seq/setup.R
+++ appyters/ChIP_seq/setup.R
@@ -2,4 +2,11 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
 BiocManager::install("org.Hs.eg.db")
+BiocManager::install("org.Mm.eg.db")
+
 BiocManager::install("ChIPseeker")
+
+BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
+BiocManager::install("TxDb.Hsapiens.UCSC.hg38.knownGene")
+BiocManager::install("TxDb.Mmusculus.UCSC.mm9.knownGene")
+BiocManager::install("TxDb.Mmusculus.UCSC.mm10.knownGene")
